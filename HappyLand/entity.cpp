949214a757c9581a5adec63b01cf176e98d50b86
Henrik Lievonen <henrik.lievonen@hotmail.com>
2014-03-04 22:48:44 +0200

    Made entity.cpp compatible with (again) new math library interface.

--- HappyLand/entity.cpp
+++ HappyLand/entity.cpp
@@ -31,7 +31,7 @@ void modelEntity::updateBoneMatrices() {
 		mat4 &nodeMat = boneMatrices[i];
 		bone &node = bones[i];
 
-		mat4 boneMat = status.versor.getMatrix();
+		mat4 boneMat = mat4(status.versor.getMatrix());
 		if (node.parentIndex == (unsigned short)-1) {			// The root
 			nodeMat = boneMat;
 			continue;
